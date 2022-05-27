3042c6ec694a2595f8bb4df480f1522c1d4047cd
eeeds <encinaesteban27@gmail.com>
2022-05-27 16:40:37 -0400

    Number of parameters

--- 2-Experiment-tracking/homework/train.py
+++ 2-Experiment-tracking/homework/train.py
@@ -1,6 +1,8 @@
 import argparse
 import os
 import pickle
+from random import Random
+import mlflow
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
@@ -12,15 +14,16 @@ def load_pickle(filename: str):
 
 
 def run(data_path):
-
-    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
-    X_valid, y_valid = load_pickle(os.path.join(data_path, "valid.pkl"))
-
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_valid)
-
-    rmse = mean_squared_error(y_valid, y_pred, squared=False)
+    mlflow.set_tracking_uri("sqlite:///mlflow.db")
+    mlflow.set_experiment("homework-experiment")
+    with mlflow.start_run():
+        mlflow.sklearn.autolog()
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_valid, y_valid = load_pickle(os.path.join(data_path, "valid.pkl"))
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_valid)
+        rmse = mean_squared_error(y_valid, y_pred, squared=False)
 
 
 if __name__ == '__main__':
